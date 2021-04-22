31e95103d777fd1f1c16de962ea8c169fb3c7de6
Pawel Slapczynski <pawel.slapczynski@student.wat.edu.pl>
2021-04-22 11:23:56 +0200

    Zakończenie projektu

---
+++ JavaApp/src/pl/edu/wat/wel/javaapp/CmdType.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.wat.wel.javaapp;
+
+/**
+ *
+ * @author Paweł
+ */
+public enum CmdType {
+  RST,
+  LED,
+  RGB,
+  LCD,
+  SEG,
+  JOY,
+  ACC,
+  MAG,
+  TEM,
+  ADC,
+  MIC,
+  SPK,
+  MOT,
+  MEM
+  
+
+  
+}
---
+++ JavaApp/src/pl/edu/wat/wel/javaapp/Protocol.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.wat.wel.javaapp;
+
+/**
+ *
+ * @author Paweł
+ */
+public class Protocol {
+    
+
+    static String getCmd(int selectedCmd, String cmdValue, String cmdText, boolean cmdTypeSet) {
+     StringBuilder cmd = new StringBuilder(":");
+     CmdType selectedCmdType = CmdType.values()[selectedCmd];
+     
+     cmd.append(selectedCmdType.toString());
+     
+     if(cmdTypeSet){
+     cmd.append("=");
+     cmd.append(cmdValue);
+     cmd.append("\r\n");
+     
+     switch ( selectedCmdType){
+         
+         case LCD:
+             cmd.append(cmdText);
+             cmd.append("\0");
+             break;
+     }
+     }
+     else{
+          cmd.append("?");
+          cmd.append("\r\n");
+     }
+     return cmd.toString();
+    }
+    
+    static boolean checkResponse(int selectedCmd, String response) {
+        
+    if(response.contains("OK\r\n")){
+      return true;
+    }
+    else if (response.contains("ERROR")){
+      return false;
+    }
+    else
+    {
+    return false;
+    }
+    }
+}
